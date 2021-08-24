8028b02835aa911c94145a609a4a41a903a85014
Ayush Jain <ayushjain4484@gmail.com>
2021-08-24 11:27:34 +0200

    Header update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,24 @@
 import React from "react"
 import Typed from "react-typed"
+import video from "../header.mp4"
 
 const Header = () => {
     return (
+        <div>
+            <video autoPlay muted loop id="myVideo"><source src={video} type="video/mp4"/></video>
         <div className="header-wraper">
             <div className="main-info">
-                <h1>web development and make sense of the data</h1>
+                <h1>AYUSH JAIN</h1>
                 <Typed
                     className="typed-text"
                     strings={["Web Development","Data Science","Full Stack Development"]}
                     typeSpeed={40}
                     backSpeed={60}
                     loop
-                />
+                    />
                 <a href="#" className="btn-main-offer">Contact Me</a>
             </div>
+        </div>
         </div>
     )
 }
